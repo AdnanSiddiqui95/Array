36f5b6452a922b764f8ccf911774ccdc665e2dea
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-15 18:00:47 +0530

    Add files via upload

---
+++ AA1.java
@@ -0,0 +1,16 @@
+
+public class AA1 
+{
+
+	public static void main(String[] args) {
+		int [] arr=new int[]  {12,23,34,45,67};
+		for(int x=0;x<arr.length;x++)
+		{
+			System.out.println(arr[x]);
+		}
+		
+		
+
+	}
+
+}
